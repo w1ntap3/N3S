fb5f6f7e840a8320146469aeee9596bba0ff6e81
w1ntap3 <39618610+w1ntap3@users.noreply.github.com>
2026-08-08 17:29:31 +0400

    header table improvement

--- src/lib/slot.c
+++ src/lib/slot.c
@@ -1,51 +1,214 @@
+// 2) finish free
+// 3) when occupying, fill rest of name with ' '
 #include "slot.h"
+#include "avr/sfr_defs.h"
 #include "eeprom.h"
 #include <stdint.h>
+#include <string.h>
 
-static void update_busy_slots(uint8_t *slot_row) {
-  uint8_t is_busy = (slot_row[1] != 0);
-  uint8_t type = (slot_row[0] >> 6);
-  uint8_t slot_number = slot_row[0] & 0b00111111;
+static const uint8_t metadata[HEADER_METADATA_BYTES] = {
+	'A', 'Z', 'E', HEADER_VERSION, 0, 0, 0, 0,
+};
 
-  if (is_busy) {
-    busy_slots[type] |= (1ULL << slot_number);
-  } else {
-    busy_slots[type] &= ~(1ULL << slot_number);
-  }
+static const uint8_t free_name[SLOT_MAX_FILENAME] = {0};
+
+static const uint16_t section_address[HEADER_SECTIONS] = {
+	DATA_ADDRESS,
+	DATA_ADDRESS + SECTION_SIZE,
+	DATA_ADDRESS + 2 * SECTION_SIZE,
+	DATA_ADDRESS + 3 * SECTION_SIZE,
+};
+
+uint64_t busy_slots[HEADER_SECTIONS] = {0};
+uint8_t headers_initialized = 0;
+
+static slot_ret_t slot_find_by_name(const uint8_t *name,
+				    const slot_section_t section,
+				    uint8_t *slots_array,
+				    const uint8_t slots_array_size,
+				    uint8_t *found_count) {
+	if (!name || !slots_array || !found_count)
+		return SL_NULL_ARGUMENTS;
+	if (section >= HEADER_SECTIONS)
+		return SL_INVALID_SECTION;
+	for (uint8_t c = 0; c < SLOT_MAX_FILENAME; c++) {
+		if (name[c] == 0)
+			return SL_INVALID_FILENAME;
+	}
+
+	uint8_t inspected_slot[SLOT_MAX_FILENAME] = {0};
+	uint8_t current_element = 0;
+	*found_count = 0;
+	for (uint8_t slot_num = 0; slot_num < SLOT_PER_SECTION; slot_num++) {
+		eeprom_read(TABLE_ADDRESS +
+				    section * SLOT_PER_SECTION *
+					    SLOT_MAX_FILENAME +
+				    slot_num * SLOT_MAX_FILENAME,
+			    inspected_slot, SLOT_MAX_FILENAME);
+		if (inspected_slot[0] == 0)
+			continue;
+		if (!memcmp(inspected_slot, name, SLOT_MAX_FILENAME)) {
+			if (current_element >= slots_array_size)
+				return SL_INSUFFICIENT_SIZE;
+			slots_array[current_element] = slot_num;
+			current_element++;
+			*found_count = current_element;
+		}
+	}
+	if (current_element == 0)
+		return SL_NOT_FOUND;
+	return SL_OK;
+}
+
+static inline void update_slot_busy(uint16_t slot, uint8_t busy) {
+	uint8_t section = slot / SLOT_PER_SECTION;
+	uint8_t bit = slot % SLOT_PER_SECTION;
+
+	if (busy)
+		busy_slots[section] |= (1ULL << bit);
+	else
+		busy_slots[section] &= ~(1ULL << bit);
+}
+
+static inline slot_ret_t check_filename(const uint8_t *name) {
+	for (uint8_t c = 0; c < SLOT_MAX_FILENAME; c++) {
+		if (name[c] == 0)
+			return SL_INVALID_FILENAME;
+	}
+	return SL_OK;
 }
 
 slot_ret_t slot_header_init(void) {
-  // Check Metadata Bytes
-  uint8_t metadata[HEADER_BYTES];
-  eeprom_read(0, metadata, HEADER_BYTES);
-  // Set busy_slots
-  for (int i = 0; i < HEADER_MAX_SLOTS; i++) {
-    uint8_t slot_row[SLOT_SIZE];
-    eeprom_read(HEADER_ADDRESS + i * SLOT_SIZE, slot_row, SLOT_SIZE);
-    update_busy_slots(slot_row);
-  }
-  return SL_OK;
+	headers_initialized = 0;
+	// Check Metadata Bytes
+	uint8_t hardware_metadata[HEADER_METADATA_BYTES];
+	eeprom_read(0, hardware_metadata, HEADER_METADATA_BYTES);
+	for (uint8_t i = 0; i < HEADER_METADATA_BYTES; i++) {
+		if (metadata[i] != hardware_metadata[i])
+			return SL_WRONG_METADATA;
+	}
+	// Set busy_slots
+	for (uint16_t current_slot = 0; current_slot < HEADER_MAX_SLOTS;
+	     current_slot++) {
+		uint8_t slot_row[SLOT_MAX_FILENAME];
+		eeprom_read(TABLE_ADDRESS + current_slot * SLOT_MAX_FILENAME,
+			    slot_row, SLOT_MAX_FILENAME);
+		update_slot_busy(current_slot, slot_row[0] != 0);
+	}
+	headers_initialized = 1;
+	return SL_OK;
 }
 
 void slot_table_format(void) {
-  // Write Metadata Bytes
-  uint8_t metadata[HEADER_BYTES] = {0};
-  metadata[0] = HEADER_MAGIC0;
-  metadata[1] = HEADER_MAGIC1;
-  metadata[2] = HEADER_MAGIC2;
-  metadata[3] = HEADER_VERSION;
-  metadata[4] = 0; // reserved
-  metadata[5] = 0; // reserved
-  metadata[6] = 0; // reserved
-  metadata[7] = 0; // reserved
-  eeprom_write(0, metadata,
-               HEADER_BYTES); // fix overwriting top of the table
+	// Write Metadata Bytes
+	eeprom_write(0, metadata, HEADER_METADATA_BYTES);
 
-  // Reset the table
+	// Reset the table
+	uint8_t reset_page[EEPROM_PAGE_SIZE] = {0};
+	for (uint16_t addr = TABLE_ADDRESS;
+	     addr < TABLE_ADDRESS + HEADER_MAX_SLOTS * SLOT_MAX_FILENAME;
+	     addr += EEPROM_PAGE_SIZE) {
+		eeprom_write(addr, reset_page, EEPROM_PAGE_SIZE);
+	}
 }
 
-uint8_t slot_occupy(Slot new_slot);
-slot_ret_t slot_free(uint8_t type, uint8_t slot);
-slot_ret_t slot_write(uint8_t type, uint8_t slot, const uint8_t *data);
-slot_ret_t slot_read(uint8_t type, uint8_t slot, uint8_t *buf);
-uint8_t slot_find(const uint8_t type, const char *name, uint8_t *slots_array);
+slot_ret_t slot_occupy(const Slot *new_slot, const slot_section_t section,
+		       uint8_t *pos_in_section) {
+	// iterate through busy slots in the right section
+	// find a free slot (0-63), fill it up with name on eeprom, return
+	// position (as a parameter), update busy_slots
+	if (!headers_initialized)
+		return SL_NOT_INITIALIZED;
+	if (!new_slot || !pos_in_section)
+		return SL_NULL_ARGUMENTS;
+	if (section >= HEADER_SECTIONS)
+		return SL_INVALID_SECTION;
+	if (check_filename(new_slot->name) == SL_INVALID_FILENAME)
+		return SL_INVALID_FILENAME;
+	for (uint8_t current_slot = 0; current_slot < SLOT_PER_SECTION;
+	     current_slot++) {
+		uint8_t is_busy = (busy_slots[section] >> current_slot) & 1;
+		if (!is_busy) {
+			eeprom_write(TABLE_ADDRESS +
+					     SLOT_PER_SECTION * section *
+						     SLOT_MAX_FILENAME +
+					     current_slot * SLOT_MAX_FILENAME,
+				     new_slot->name, SLOT_MAX_FILENAME);
+			*pos_in_section = current_slot;
+			update_slot_busy(
+				section * SLOT_PER_SECTION + current_slot, 1);
+			return SL_OK;
+		}
+	}
+	return SL_SECTION_BUSY;
+}
+slot_ret_t slot_free(const Slot *slot, const slot_section_t section) {
+	// Check for errors
+	if (!headers_initialized)
+		return SL_NOT_INITIALIZED;
+	if (section >= HEADER_SECTIONS)
+		return SL_INVALID_SECTION;
+	if (!slot)
+		return SL_NULL_ARGUMENTS;
+	slot_ret_t err = check_filename(slot->name);
+	if (err == SL_INVALID_FILENAME)
+		return SL_INVALID_FILENAME;
+	uint8_t slots_array[SLOT_PER_SECTION];
+	uint8_t found_count;
+	err = slot_find_by_name(slot->name, section, slots_array,
+				SLOT_PER_SECTION, &found_count);
+	if (err != SL_OK)
+		return err;
+
+	// Logic
+	uint8_t slot_to_free = slots_array[found_count - 1];
+	eeprom_write(TABLE_ADDRESS +
+			     section * SLOT_PER_SECTION * SLOT_MAX_FILENAME +
+			     slot_to_free * SLOT_MAX_FILENAME,
+		     free_name, SLOT_MAX_FILENAME);
+	update_slot_busy(section * SLOT_PER_SECTION + slot_to_free, 0);
+	return SL_OK;
+}
+
+slot_ret_t slot_write(const slot_section_t section, const Slot *slot,
+		      const uint8_t *data, const uint16_t data_size) {
+	if (!headers_initialized)
+		return SL_NOT_INITIALIZED;
+	if (section >= HEADER_SECTIONS)
+		return SL_INVALID_SECTION;
+	if (!slot)
+		return SL_NULL_ARGUMENTS;
+	slot_ret_t err = check_filename(slot->name);
+	if (err != SL_OK)
+		return err;
+
+	uint8_t slots_array[SLOT_PER_SECTION];
+	uint8_t found_count;
+	err = slot_find_by_name(slot->name, section, slots_array,
+				SLOT_PER_SECTION, &found_count);
+	if (err != SL_OK)
+		return err;
+	// if statement above checks for it but implicit trust sucks
+	if (found_count == 0)
+		return SL_NOT_FOUND;
+
+	// Logic
+	uint8_t write_slot = slots_array[found_count - 1];
+	uint16_t slots_to_write = (uint16_t)data_size / (uint16_t)SLOT_SIZE;
+
+	for (uint16_t i = 0; i < slots_to_write; i++) {
+		uint16_t write_until = section_address[section] +
+				       write_slot * SLOT_SIZE +
+				       slots_to_write * SLOT_SIZE;
+		if (write_until >
+		    section_address[section] + SLOT_SIZE * SLOT_PER_SECTION)
+			return SL_NOT_ENOUGH_SPACE;
+
+		eeprom_write(section_address[section] + write_slot * SLOT_SIZE +
+				     i * SLOT_SIZE,
+			     data + i * SLOT_SIZE, SLOT_SIZE);
+	}
+	return SL_OK;
+}
+slot_ret_t slot_read(const slot_section_t section, const Slot *slot,
+		     uint8_t *buf, const uint8_t buf_size);
--- src/lib/slot.h
+++ src/lib/slot.h
@@ -4,47 +4,72 @@
 #include <stdint.h>
 
 // Metadata
-#define HEADER_MAGIC0 0x41
-#define HEADER_MAGIC1 0x5A
-#define HEADER_MAGIC2 0x45
-#define HEADER_VERSION 0 // 0-255
-#define HEADER_BYTES 8
-// Header table
-#define HEADER_ADDRESS 0x08
-#define SLOT_MAX_FILENAME 7
-#define SLOT_TXT_TYPE 0b00
-#define SLOT_EXE_TYPE 0b01
-#define SLOT_VEC_TYPE 0b10
-#define SLOT_IMG_TYPE 0b11
+#define HEADER_VERSION 1 // 0-255
+#define HEADER_METADATA_BYTES 8
+// Header segment
+// #define HEADER_ADDRESS 0x00
+#define TABLE_ADDRESS 0x08
+#define SLOT_MAX_FILENAME 8
 #define SLOT_PER_SECTION 64
-#define SLOT_SIZE (SLOT_MAX_FILENAME + 1) // name bytes + type:slot byte = 8
+#define SLOT_SIZE 122
 #define HEADER_SECTIONS 4
 #define HEADER_MAX_SLOTS (SLOT_PER_SECTION * HEADER_SECTIONS)
+// Data segment
+#define DATA_ADDRESS                                                           \
+	HEADER_METADATA_BYTES +                                                \
+		HEADER_SECTIONS * SLOT_PER_SECTION * SLOT_MAX_FILENAME
+#define SECTION_SIZE (SLOT_SIZE * SLOT_PER_SECTION)
+
+typedef enum {
+	SL_OK = 0,
+	SL_WRONG_METADATA,
+	SL_INVALID_FILENAME,
+	SL_INVALID_SECTION,
+	SL_INSUFFICIENT_SIZE,
+	SL_NOT_FOUND,
+	SL_SECTION_BUSY,
+	SL_NOT_INITIALIZED,
+	SL_NULL_ARGUMENTS,
+	SL_NOT_ENOUGH_SPACE
+} slot_ret_t;
+
+typedef enum {
+	SLOT_TXT_SECTION = 0b00,
+	SLOT_EXE_SECTION = 0b01,
+	SLOT_VEC_SECTION = 0b10,
+	SLOT_IMG_SECTION = 0b11
+} slot_section_t;
+
+typedef enum {
+	SECTION_TXT_ADDRESS = DATA_ADDRESS,
+	SECTION_EXE_ADDRESS = DATA_ADDRESS + SECTION_SIZE,
+	SECTION_VEC_ADDRESS = DATA_ADDRESS + 2 * SECTION_SIZE,
+	SECTION_IMG_ADDRESS = DATA_ADDRESS + 3 * SECTION_SIZE,
+} data_section_t;
 
 typedef struct {
-  uint8_t type_and_sectionpos;  // 0:1 bits for type; 2:7 for slot number
-                                // (relative to section)
-  char name[SLOT_MAX_FILENAME]; // non-null terminated string
+	uint8_t name[SLOT_MAX_FILENAME]; // non-null terminated string
 } Slot;
-
-typedef enum { SL_OK = 0 } slot_ret_t;
 
 // busy_slots[0] = text
 // busy_slots[1] = exe
 // busy_slots[2] = vec
 // busy_slots[3] = img
 extern uint64_t busy_slots[HEADER_SECTIONS];
+extern uint8_t headers_initialized;
 
+// software is now aware of header. never writes into hardware and only reads
+// from it.
 slot_ret_t slot_header_init(void);
-// make all names null strings
+// hardware level reset of header (by making all names null strings). never
+// touches software and only reads from it.
 void slot_table_format(void);
 
-// 0 - no free slots; returns 1-255 if assigned a slot
-uint8_t slot_occupy(Slot new_slot);
-slot_ret_t slot_free(uint8_t type, uint8_t slot);
-slot_ret_t slot_write(uint8_t type, uint8_t slot, const uint8_t *data);
-slot_ret_t slot_read(uint8_t type, uint8_t slot, uint8_t *buf);
-// 0 - no slots found; returns 1-255 if found a slot
-uint8_t slot_find(const uint8_t type, const char *name, uint8_t *slots_array);
-
+slot_ret_t slot_occupy(const Slot *new_slot, const slot_section_t section,
+		       uint8_t *pos_in_section);
+slot_ret_t slot_free(const Slot *slot, const slot_section_t section);
+slot_ret_t slot_write(const slot_section_t section, const Slot *slot,
+		      const uint8_t *data, const uint16_t data_size);
+slot_ret_t slot_read(const slot_section_t section, const Slot *slot,
+		     uint8_t *buf, const uint8_t buf_size);
 #endif
