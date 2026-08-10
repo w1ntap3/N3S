56d7e55a74356fdb3b96938b7756216e54048eea
w1ntap3 <39618610+w1ntap3@users.noreply.github.com>
2026-08-10 23:36:38 +0400

    purged slot.h messy architecture

--- src/lib/slot.h
+++ src/lib/slot.h
@@ -3,35 +3,22 @@
 
 #include <stdint.h>
 
-// Metadata
-#define HEADER_VERSION 1 // 0-255
+#define HEADER_VERSION 2 // 0-255
 #define HEADER_METADATA_BYTES 8
-// Header segment
-// #define HEADER_ADDRESS 0x00
-#define TABLE_ADDRESS 0x08
+#define SLOT_ENTRIES_ADDRESS 0x08
 #define SLOT_MAX_FILENAME 8
-#define SLOT_PER_SECTION 64
-#define SLOT_SIZE 122
+#define SLOTS_PER_SECTION 64
+#define SLOT_ENTRY_SIZE 10
+#define SLOT_DATA_BYTES 114
 #define HEADER_SECTIONS 4
-#define HEADER_MAX_SLOTS (SLOT_PER_SECTION * HEADER_SECTIONS)
+#define HEADER_MAX_SLOTS (SLOTS_PER_SECTION * HEADER_SECTIONS)
 // Data segment
 #define DATA_ADDRESS                                                           \
 	HEADER_METADATA_BYTES +                                                \
-		HEADER_SECTIONS * SLOT_PER_SECTION * SLOT_MAX_FILENAME
-#define SECTION_SIZE (SLOT_SIZE * SLOT_PER_SECTION)
+		HEADER_SECTIONS * SLOTS_PER_SECTION * SLOT_ENTRY_SIZE
+#define SECTION_SIZE (SLOT_DATA_BYTES * SLOTS_PER_SECTION)
 
-typedef enum {
-	SL_OK = 0,
-	SL_WRONG_METADATA,
-	SL_INVALID_FILENAME,
-	SL_INVALID_SECTION,
-	SL_INSUFFICIENT_SIZE,
-	SL_NOT_FOUND,
-	SL_SECTION_BUSY,
-	SL_NOT_INITIALIZED,
-	SL_NULL_ARGUMENTS,
-	SL_NOT_ENOUGH_SPACE
-} slot_ret_t;
+typedef enum { SL_OK = 0, SL_WRONG_METADATA } slot_ret_t;
 
 typedef enum {
 	SLOT_TXT_SECTION = 0b00,
@@ -48,7 +35,9 @@ typedef enum {
 } data_section_t;
 
 typedef struct {
-	uint8_t name[SLOT_MAX_FILENAME]; // non-null terminated string
+	uint8_t name[SLOT_MAX_FILENAME];
+	uint8_t order;
+	uint8_t write_offset;
 } Slot;
 
 // busy_slots[0] = text
@@ -56,7 +45,7 @@ typedef struct {
 // busy_slots[2] = vec
 // busy_slots[3] = img
 extern uint64_t busy_slots[HEADER_SECTIONS];
-extern uint8_t headers_initialized;
+extern uint8_t slots_initialized;
 
 // software is now aware of header. never writes into hardware and only reads
 // from it.
@@ -65,11 +54,11 @@ slot_ret_t slot_header_init(void);
 // touches software and only reads from it.
 void slot_table_format(void);
 
-slot_ret_t slot_occupy(const Slot *new_slot, const slot_section_t section,
+slot_ret_t slot_occupy(const char *name, const slot_section_t section,
 		       uint8_t *pos_in_section);
-slot_ret_t slot_free(const Slot *slot, const slot_section_t section);
-slot_ret_t slot_write(const slot_section_t section, const Slot *slot,
-		      const uint8_t *data, const uint16_t data_size);
-slot_ret_t slot_read(const slot_section_t section, const Slot *slot,
+slot_ret_t slot_free(const slot_section_t section, const uint8_t entry);
+slot_ret_t slot_write(const slot_section_t section, const uint8_t entry,
+		      const uint8_t *data, const uint8_t data_size);
+slot_ret_t slot_read(const slot_section_t section, const uint8_t entry,
 		     uint8_t *buf, const uint8_t buf_size);
 #endif
